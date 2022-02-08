76b7baae17afb252c3fd3c0f2e90abc4879b4d84
abumdnayeemkhan <abumohammadnayemkhan@gmail.com>
2022-02-08 19:39:15 +0600

    Add new controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,12 @@ class HomeController extends Controller
     public function index(){
         return view("home");
     }
+    public function redirect(){
+        $usertype = Auth::user()->usertype;
+        if($usertype =='1'){
+            return view('admin.adminhome');
+        }else{
+            return view('home');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get('/', [HomeController::class, "index"]);
+
+Route::get('/redirect', [HomeController::class, "redirect"]);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
